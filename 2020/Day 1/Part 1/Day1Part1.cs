764bcd1245f371003450911a88339701024e2f97
TarjeiM <moland.tarjei@gmail.com>
2024-03-06 20:43:43 +0100

    png swap for solution

--- 2020/Day 1/Part 1/Day1Part1.cs
+++ 2020/Day 1/Part 1/Day1Part1.cs
@@ -1,1 +1,24 @@
-hello world!
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string[] lines = File.ReadLines("filepath.txt").ToArray();
+        int[] myInts = Array.ConvertAll(lines, s => Int32.Parse(s));
+        for (int i = 0; i < myInts.Length; i++) 
+        {
+            for (int j = 1; j < myInts.Length; j++) 
+            {
+                if (myInts[i] + myInts[j] == 2020)
+                {
+                    Console.WriteLine($"The answer is: {myInts[i] * myInts[j]}");
+                    return;
+                }
+            }
+        }
+    }
+}
